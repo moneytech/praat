303bdf3632febfed75c1a262c2e641f80d9bc82c
Ico Doornekamp <ico@pruts.nl>
2014-10-08 13:03:05 +0200

    Flop

--- praat.c
+++ praat.c
@@ -43,12 +43,9 @@ int main(int argc, char **argv)
 	
 	int sock = socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0);
 		
-	int nfds = snd_pcm_poll_descriptors_count(pcm_rec) + 1;
+	int nfds = snd_pcm_poll_descriptors_count(pcm_rec);
 	pfds = calloc(nfds, sizeof *pfds);
-	snd_pcm_poll_descriptors(pcm_rec,  pfds, nfds-1);
-
-	pfds[nfds-1].fd = sock;
-	pfds[nfds-1].events = POLLIN;
+	snd_pcm_poll_descriptors(pcm_rec,  pfds, nfds);
 	
 	for(;;) {
 
@@ -69,33 +66,19 @@ int main(int argc, char **argv)
 
 			r = poll(pfds, nfds, -1);
 
-			/*
-			 * Read socket data 
-			 */
 
-			if(pfds[nfds-1].revents & POLLIN) {
-				int r = recv(sock, data, sizeof data, 0);
-				if(r == -1) {
-					fprintf(stderr, "recv(): %s\n", strerror(errno));
-					exit(1);
-				}
-				speex_bits_read_from(&dec_bits, data, r);
-				r = speex_decode_int(dec_state, &dec_bits, audio); 
-				
-				r = snd_pcm_writei(pcm_play, audio, frame_size);
-			}
-
-			/*
-			 * Read audio data
-			 */
-
-			snd_pcm_poll_descriptors_revents(pcm_rec, pfds, nfds-1, &revents);
+			snd_pcm_poll_descriptors_revents(pcm_rec, pfds, nfds, &revents);
 
 			if(revents & POLLERR) {
 				xrun = 1;
 			}
 
 			if(revents & POLLIN) {
+
+				/*
+				 * Read audio data
+				 */
+
 				r = snd_pcm_readi(pcm_rec, audio, frame_size);
 
 				speex_bits_reset(&enc_bits);
@@ -110,6 +93,22 @@ int main(int argc, char **argv)
 					}
 				}
 
+				/*
+				 * Read socket data
+				 */
+
+				int r = recv(sock, data, sizeof data, 0);
+				if(r > 0) {
+					speex_bits_read_from(&dec_bits, data, r);
+					r = speex_decode_int(dec_state, &dec_bits, audio); 
+					
+					r = snd_pcm_writei(pcm_play, audio, frame_size);
+				} else {
+					if(errno != EAGAIN) {
+						fprintf(stderr, "recv(): %s\n", strerror(errno));
+						exit(1);
+					}
+				}
 			}
 		}
 	}
@@ -149,6 +148,8 @@ snd_pcm_t *alsa_open(const char *devname, unsigned samplerate, snd_pcm_uframes_t
 	TRY(snd_pcm_hw_params_set_period_size_near, pcm, hwparams, &fragsize, 0);
 	TRY(snd_pcm_hw_params_set_periods_near, pcm, hwparams, &nfrags, 0);
         TRY(snd_pcm_hw_params, pcm, hwparams);
+
+	printf("%d hz\n", samplerate);
 
 	/* Software params */
 	
